e475d688130ecc0704fb06c5e6bfe4e617abe561
Bonaparte <hello@bonapartedigital.com>
2023-09-13 01:54:36 -0300

    Placeholder Website
    While the final website is being created we are deploying a placeholder basic landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,22 @@ module.exports = {
     `./src/components/**/*.{js,jsx,ts,tsx}`,
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'olive': '#C0D22D',
+        'green': '#14271D',
+        'orange': '#EC8602',
+      },
+      fontFamily: {
+        raleway: ["'Raleway', sans-serif"],
+        mulish: ["'Mulish', sans-serif"],
+      },
+      backgroundImage: {
+        'gradient-hover': 'linear-gradient(to right, #4CAF50, #8BC34A)',
+      },
+    },
   },
   plugins: [],
 }
